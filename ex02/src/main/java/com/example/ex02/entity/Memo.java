3ce1fa17752f5bb29de9986fe723eae2f3eb9e29
JeongInu <itsiuj92@gmail.com>
2025-03-17 15:14:37 +0900

    EX02

--- ex02/src/main/java/com/example/ex02/entity/Memo.java
+++ ex02/src/main/java/com/example/ex02/entity/Memo.java
@@ -1,4 +1,22 @@
 package com.example.ex02.entity;
 
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name= "tbl_memo")
+@ToString
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class Memo {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long mno;
+
+    @Column(length=200, nullable = false)
+    private String memoText;
+
 }
